291cfed57c56276152f1902e4aeaec8afacc36bc
Beatriz-ux <beatrizpereira.ld@gmail.com>
2023-10-22 19:42:11 -0300

    feat: footer com o meu nome

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './footer.module.css'
+export default function Footer () {
+  return (
+    <div className={styles.footer}>
+        <span>Desenvolvido por <a href="https://github.com/Beatriz-ux">Beatriz</a></span>
+    </div>
+  )
+}
