59cd1e530767145f63c96fc330ddef4213fdfc46
Beatriz-ux <beatrizpereira.ld@gmail.com>
2023-11-05 20:32:41 -0300

    feat: função com checagem de OutDate para acompanhar com o horário da máquina

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -3,6 +3,7 @@ import styles from "./table.module.css";
 import horarios from "@/models/horarios";
 import { capitalize } from "@/utils/formatString";
 import { BsClockHistory } from "react-icons/bs";
+import { MagicMotion } from "react-magic-motion";
 
 type Props = {
   name: string;
@@ -10,6 +11,20 @@ type Props = {
 };
 
 export default function Table(props: Props) {
+
+  function checkOutdated(horario: string) {
+    let horarioAtual = new Date().getHours();
+    let minutosAtual = new Date().getMinutes();
+    let horarioOnibus = parseInt(horario.split(":")[0]);
+    let minutosOnibus = parseInt(horario.split(":")[1]);
+
+    let minutosAtualTotal = horarioAtual * 60 + minutosAtual;
+    let minutosOnibusTotal = horarioOnibus * 60 + minutosOnibus;
+
+
+    return ((minutosAtualTotal-minutosOnibusTotal) > 60);
+  }
+
   return (
     <div className={styles.tabela}>
       <div className={styles.cabecalho}>
@@ -26,14 +41,15 @@ export default function Table(props: Props) {
           <p>Empresa</p>
         </div>
       </div>
-
+      <MagicMotion>
       <div className={styles.conteudo}>
         {horarios.map((horario, index) =>
           props.name == horario.origem && props.dia == horario.dia ? (
             <div
               key={index}
               className={styles.linha}
-              data-outdated={!!horario.outdated}
+              data-outdated={checkOutdated(horario.horario)}
+              //data-outdated={!!horario.outdated}
             >
               <div>
                 <p>{horario.horario}</p>
@@ -51,6 +67,7 @@ export default function Table(props: Props) {
           ) : null
         )}
       </div>
+      </MagicMotion>
     </div>
   );
 }
